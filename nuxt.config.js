6b6d168ea74e42cba5f5e660cd821292a0da4b55
Rockwell Schrock <schrockwell@gmail.com>
2020-09-01 13:55:55 -0400

    Fixing CSS config and extracting CSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,29 @@
 export default {
-  css: ["~/assets/css/main.css"],
+  // Fully-static site generation
+  target: "static",
+
+  // Put all components in the global namespace
+  components: true,
+
+  // Don't use CSS @import!!
+  css: [
+    "~/assets/css/fonts.css",
+    "~/assets/css/cv-prose.css",
+    "~/assets/css/main.css"
+  ],
+
+  build: {
+    extractCSS: true
+  },
+
+  // Plugins
   buildModules: ["@nuxtjs/tailwindcss", "@nuxt/content"],
+
+  // Mobile scaling stuff
   head: {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" }
     ]
-  },
-  components: true,
-  target: "static"
+  }
 };
